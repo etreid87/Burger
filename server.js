ecdd997b4fe911009e1e3076bd03177a7f975a2c
Evan Reid <etreid87@gmail.com>
2020-11-11 19:05:58 -0500

    server config db setup

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
